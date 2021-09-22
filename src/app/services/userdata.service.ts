21004157c7e45f30a72720acd9c4c9063c10bcdd
Mary <mary_hbb@yahoo.com.com>
2021-09-22 23:43:32 +0200

    add Johannes to user

--- src/app/services/userdata.service.ts
+++ src/app/services/userdata.service.ts
@@ -11,6 +11,8 @@ user(){
     {name: 'Mani', age: 15, email: 'mani@test.com'},
     {name: 'Arvin', age: 14, email: 'arvin@test.com'},
     {name: 'Radin', age: 12, email: 'radin@test.com'},
+    {name: 'Johannes', age: 38, email: 'johan@test.com'},
+
   ]
 }
 
